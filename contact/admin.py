9184b35f816ff3e0866d3dff41e9ee0b5fcce1ec
fabiomagajr <fabiomagajr@gmail.com>
2025-02-16 14:59:13 -0300

    admin and models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from contact import models
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name','last_name','phone',
+    ordering = '-id',
+    list_filter = 'created_date',
+    search_fields = 'first_name','last_name',
+    
